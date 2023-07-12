ea76b46d75197396db883271953895b30fceca10
student <student@BA.local>
2023-07-12 11:56:43 +0300

    first commit in this branch

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 const express= require('express')
 const App= express()
 const mongoose=require('mongoose')
-
+// ......................................................
 
 const bodyParser=require('body-parser')
 App.use(bodyParser.json())
